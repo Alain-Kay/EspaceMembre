8e335c38c937b2cbe0784eb870c8975b465fd079
Alain KAY <kazadikayumba@gmail.com>
2022-04-04 12:48:39 +0200

    mise en forme du script profile.php

--- profile.php
+++ profile.php
@@ -3,4 +3,13 @@ session_start();
 require_once "_inc/header.php";
 ?>
 
-<h3>Bonjour <?= ucfirst($_SESSION['user']); ?>
+<div class="container">
+    <div class="row">
+        <div class="col mt-4">
+            <h3>Bonjour <?= ucfirst($_SESSION['user']); ?>
+        </div> 
+        <div class="col mt-4 ">
+            <a href ="deconnexion.php" class="btn btn-danger btn-lg">Deconnexion </a>
+        </div>
+    </div>
+</div>
